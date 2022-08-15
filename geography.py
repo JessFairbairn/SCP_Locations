8968ef87793ed403ea5a6df7d91333c6eea4d82e
Jess <jessfairbairn91@gmail.com>
2022-08-15 22:42:07 +0100

    Remove leading 'the' from location names

--- geography.py
+++ geography.py
@@ -9,24 +9,27 @@ from database import insert_location, retrieve_location
 geolocator = Nominatim(user_agent="example app")
 
 def get_coordinates(conn:sqlite3.Connection, location_name: str) -> Tuple[float, float]:
-        geo_data = retrieve_location(conn, location_name)
+    if location_name.startswith("the "):
+        location_name = location_name[4:]
 
-        if geo_data is not None:
-            return (geo_data["lat"], geo_data["lon"])
+    geo_data = retrieve_location(conn, location_name)
 
-        try:
-            response = geolocator.geocode(location_name)
-            if response is None:
-                print(f"Couldn't locate {location_name}")
-                return None
-        except GeopyError as ex:
-            print(ex)
-            return None
-
-        geo_data = response.raw
-        # geo_data = places.PlaceContext([location_list[0]])
-        # geo_data = locator.Locator().locateLocation(location_list[0])
-
-
-        insert_location(conn, location_name, geo_data)
+    if geo_data is not None:
         return (geo_data["lat"], geo_data["lon"])
+
+    try:
+        response = geolocator.geocode(location_name)
+        if response is None:
+            print(f"Couldn't locate {location_name}")
+            return None
+    except GeopyError as ex:
+        print(ex)
+        return None
+
+    geo_data = response.raw
+    # geo_data = places.PlaceContext([location_list[0]])
+    # geo_data = locator.Locator().locateLocation(location_list[0])
+
+
+    insert_location(conn, location_name, geo_data)
+    return (geo_data["lat"], geo_data["lon"])
