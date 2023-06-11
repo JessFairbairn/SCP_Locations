11d36dcaaf8958aba1d42b4605386b49e6c2eb20
Jess Fairbairn <jessfairbairn91@gmail.com>
2023-06-11 22:42:28 +0100

    Improved download script

--- download.py
+++ download.py
@@ -2,14 +2,18 @@ import os
 
 from functions import download_article, get_all_scp_links
 
-# article_name = "scp-002"
-
-# download_article(article_name)
-
 urls = get_all_scp_links()
 
 
+urls = get_all_scp_links()
+
+def process_article(article_url):
+    if article_url not in already_downloaded:
+        offset_links = download_article(article_url)
+        already_downloaded.append(article_url)
+        for link in offset_links:
+            process_article(link)
+
 already_downloaded = os.listdir("/home/furby/Code/SCP_Locations/downloads/")
 for url in urls:
-    if url not in already_downloaded:
-        download_article(url)
+    process_article(url)
--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import re
+from urllib.parse import urlparse
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -13,14 +16,38 @@ def get_all_scp_links() -> list[str]:
     links = wrapper.find_all('a')
     return list(map(lambda link_tag: link_tag.attrs['href'][1:], links))
 
-def download_article(article_name: str) -> None:
-    url = f"https://scp-wiki.wikidot.com/{article_name}"
+
+OFFSET_REGEX = re.compile(r"offset\/\d+")
+
+def download_article(article_name: str) -> list[int]:
+    """Downloads article, returns list of offset articles linked"""
+    if "scp-wiki.wikidot.net" in article_name:
+        article_name = urlparse(article_name).path
+
+    url = f"https://scp-wiki.wikidot.com/{article_name.replace('¦', '/')}"
 
     file = requests.get(url)
-    file.raise_for_status()
+    try:
+        file.raise_for_status()
+    except requests.exceptions.HTTPError as ex:
+        if ex.response.status_code == 404:
+            print(ex)
+            return []
+        else:
+            raise
 
     soup = BeautifulSoup(file.text, 'html.parser')
     content = soup.find(id='page-content')
 
-    with open(f'downloads/{article_name}', 'w') as mah_file:
+    with open(f'downloads/{article_name}', 'w', encoding="utf-8") as mah_file:
         mah_file.write(content.text)
+    offset_links = content.find_all("a", href=OFFSET_REGEX)
+    return list(map(
+        lambda link_tag: _clean_internal_link(link_tag.attrs['href']),
+        offset_links
+    ))
+
+def _clean_internal_link(url: str) -> str:
+    if url.startswith("http"):
+        url = urlparse(url).path[1:]
+    return url.replace('/','¦')
